b656c8c93dc2f3288258360e31ce9f1aaae7f416
Jakob Gillich <jakob@gillich.me>
2022-04-07 20:05:48 +0000

    graphql-yoga: use cluster

--- graphql-yoga/index.js
+++ graphql-yoga/index.js
@@ -1,20 +1,28 @@
+import cluster from 'cluster'
+import {cpus} from 'os'
 import { createServer } from '@graphql-yoga/node'
 
 process.env.PORT = "8000"
 
-const server = createServer({
-  schema: {
-    typeDefs: `
-      type Query {
-        hello: String!
-      }
-    `,
-    resolvers: {
-      Query: {
-        hello: () => 'world'
+if (cluster.isPrimary) {
+  for (let i = 0; i < cpus().length; i++) {
+    cluster.fork()
+  }
+} else {
+  const server = createServer({
+    schema: {
+      typeDefs: `
+        type Query {
+          hello: String!
+        }
+      `,
+      resolvers: {
+        Query: {
+          hello: () => 'world'
+        }
       }
     }
-  }
-})
+  })
 
-server.start()
+  server.start()
+}
