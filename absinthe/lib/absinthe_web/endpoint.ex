5b0f7b27e4a4b2dc81a95497a798a361befac9bd
Jakob Gillich <jakob@gillich.me>
2022-04-10 22:40:40 +0000

    absinthe: remove unused code

--- absinthe/lib/absinthe_web/endpoint.ex
+++ absinthe/lib/absinthe_web/endpoint.ex
@@ -1,19 +1,10 @@
 defmodule AbsintheWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :absinthe_web
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
-  if code_reloading? do
-    plug Phoenix.CodeReloader
-  end
-
   plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json, Absinthe.Plug.Parser],
+    parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
-
-  plug Plug.MethodOverride
-  plug Plug.Head
 
   plug Absinthe.Plug,
     schema: AbsintheWeb.Schema
