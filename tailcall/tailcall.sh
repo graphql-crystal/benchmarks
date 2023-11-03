05c995ab6d46b0d2e2a6f4fc510d175acb18743b
chandan singh <sc231997@gmail.com>
2023-11-03 00:45:53 +0000

    fix permission issue

--- tailcall/tailcall.sh
+++ tailcall/tailcall.sh
@@ -1,4 +1,4 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/vscode/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew tap tailcallhq/tailcall
